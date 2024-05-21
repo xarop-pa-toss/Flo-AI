47357f1e9ba6b2626070cb9de0b7e9d71bab34e1
xarop-pa-toss <rpgdsantos@gmail.com>
2024-05-21 19:58:20 +0100

    No changes

--- cmd/internal/viper/viper.go
+++ cmd/internal/viper/viper.go
@@ -1,20 +1,29 @@
 package viper
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/spf13/viper"
 )
 
-func LoadConfig(configFilePath string) error {
+func LoadConfig() error {
 	viper.SetConfigFile("C:\\Users\\Ricardo\\source\\repos\\Flo-AI_Config\\viperconfig.yaml")
 	viper.SetConfigType("yaml")
 	viper.AutomaticEnv()
 
 	if err := viper.ReadInConfig(); err != nil {
-		fmt.Println("Error reading configuration file:", err)
+		fmt.Println("Error reading configuration file: ", err)
 		return fmt.Errorf("error reading configuration file: %w", err)
 	}
 
 	return nil
+}
+
+func GetString(key string) (string, error) {
+	if viper.IsSet(key) {
+		return viper.GetString(key), nil
+	}
+
+	return "", errors.New("VIPER - Cannot find key")
 }
--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+
+	"Flo-AI/cmd/internal/viper"
+	"Flo-AI/cmd/internal/vosk"
+)
+
 func main() {
+
+	viper.LoadConfig()
+	vosk.Init()
+
+	fmt.Println("bruh")
 
 }
