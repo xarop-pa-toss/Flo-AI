2e96358076d75954669363ba469f5d40f800197c
rgiro <ricardo.giro@sisgarbe.pt>
2024-07-23 14:08:50 +0100

    Added basic Bubble Tea main menu

--- cmd/main.go
+++ cmd/main.go
@@ -8,9 +8,93 @@ import (
 	"Flo-AI/cmd/internal/openai"
 	"Flo-AI/cmd/internal/viper"
 	"Flo-AI/cmd/internal/whisperstt"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
 
+// Model for app state
+type menuModel struct {
+	header  string   // Header text
+	choices []string // Menu options
+	cursor  int      // Cursor index-based position
+	footer  string   // Footer text
+}
+
+func mainMenuModel() menuModel {
+	return menuModel{
+		header: "Welcome to Flo, the AI powered voice assistant!\n",
+		choices: []string{
+			"1 - Start Flo (with avatar)",
+			"2 - Start Flo (command line only)",
+			"3 - Preferences",
+			"4 - Exit"},
+		footer: "Ctrl+C or Q to quit...",
+	}
+}
+
+// INIT
+func (m menuModel) Init() tea.Cmd {
+	return nil
+}
+
+// UPDATE
+func (m menuModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	// Key press I/O
+	case tea.KeyMsg:
+		// Check which key
+		switch msg.String() {
+		// Exit keys
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+		}
+	}
+	// Return updated model to Bubble Tea runtime. Not ret`urning a command.
+	return m, nil
+}
+
+// VIEW
+func (m menuModel) View() string {
+	// UI is only a string. Redrawing is managed by Bubble Tea
+
+	// Header
+	s := m.header
+
+	// Iterate through choices
+	for i, choice := range m.choices {
+		// Draw cursor on selected option
+		cursor := " "
+		if m.cursor == i {
+			cursor = ">"
+		}
+
+		// Render this row by adding to the final string
+		s += fmt.Sprintf("%s %s\n", cursor, choice)
+	}
+
+	// Footer
+	s += m.footer
+
+	// Send UI to Bubble Tea for rendering
+	return s
+}
+
 func main() {
+	p := tea.NewProgram(mainMenuModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Error trying to create main menu: %v", err)
+		os.Exit(1)
+	}
+
 	// LOAD Viper
 	if err := viper.LoadConfig(); err != nil {
 		panic(err.Error())
--- go.mod
+++ go.mod
@@ -9,13 +9,24 @@ require github.com/sashabaranov/go-openai v1.26.2
 require (
 	github.com/MarkKremer/microphone v1.2.0 // indirect
 	github.com/beep/beep v0.0.0-20210427194017-48096b79e5a8 // indirect
+	github.com/charmbracelet/bubbletea v0.26.6 // indirect
+	github.com/charmbracelet/x/ansi v0.1.2 // indirect
+	github.com/charmbracelet/x/input v0.1.0 // indirect
+	github.com/charmbracelet/x/term v0.1.1 // indirect
+	github.com/charmbracelet/x/windows v0.1.0 // indirect
+	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/gopxl/beep v1.4.1 // indirect
 	github.com/gordonklaus/portaudio v0.0.0-20230709114228-aafa478834f5 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/sagikazarmark/locafero v0.6.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
@@ -23,8 +34,10 @@ require (
 	github.com/spf13/cast v1.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/exp v0.0.0-20240613232115-7f521ea00fb8 // indirect
+	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.22.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
